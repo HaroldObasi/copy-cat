6051dae7b781af89032896c3063d556d769c4150
Harold Obasi <haroldobasi2k16@gmail.com>
2024-10-26 00:10:48 +0100

    added types for create project response, and request

--- deployments/types.go
+++ deployments/types.go
@@ -16,13 +16,45 @@ type GitRepository struct {
 	Type GitRepositoryType `json:"type"`
 }
 
+type Link struct {
+	Type GitRepositoryType `json:"type"`
+	Repo string `json:"repo"`
+	RepoId int `json:"repoId"`
+}
+
 type CreateProjectRequest struct {
 	Name string `json:"name"`
 	BuildCommand string `json:"buildCommand"`
-	DevCommand string `json:"devCommand"`
 	EnableAffectedProjectsDeployments bool `json:"enableAffectedProjectsDeployments"`
 	Framework Framework `json:"framework"`
 	GitRepository GitRepository `json:"gitRepository"`
 	InstallCommand string `json:"installCommand"`
 	SkipGitConnectDuringLink bool `json:"skipGitConnectDuringLink"`
+}
+
+type CreateProjectResponse struct {
+	CreatedAt int `json:"createdAt"`
+	Link Link `json:"link"`
+}
+
+
+type GitSource struct {
+	Ref string `json:"ref"`
+	RepoId int `json:"repoId"`
+	Type GitRepositoryType `json:"type"`
+}
+
+type projectSettings struct {
+	Framework Framework `json:"framework"`
+}
+
+type DeployProjectRequest struct {
+	Name string `json:"name"`
+	GitSource GitSource `json:"gitSource"`
+	ProjectSettings projectSettings `json:"projectSettings"`
+}
+
+type DeployProjectResponse struct {
+	Alias []string `json:"alias"`
+	CreatedAt int `json:"createdAt"`
 }
